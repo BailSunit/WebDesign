3d2278d92370e173d5c532cfecc5d73b9a53b03a
BailSunit <sunitbail@yahoo.com>
2022-08-05 18:21:39 -0400

    Node changes

---
+++ Node/Wishlist/app.js
@@ -0,0 +1,9 @@
+var newTask = document.querySelector('#new-task');
+var addButton = document.querySelector('#addTask');
+
+var toDoUl = document.querySelector('.todo-list ul');
+var completeUl = document.querySelector('.complete-list ul');
+
+var addTask = function() {
+    console.log("Adding task");
+}
